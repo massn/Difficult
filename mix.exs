b61bdde3956de70b085c3a3501297be108de8e8f
Massn <massn23579@gmail.com>
2016-02-10 01:07:59 +0900

    to hex

--- mix.exs
+++ mix.exs
@@ -6,7 +6,13 @@ defmodule Difficult.Mixfile do
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod
+     start_permanent: Mix.env == :prod,
+     description: "Difficult, but computable functions",
+     package: [
+       maintainers: ["Massn"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/massn/Difficult"}
+     ]
    ]
   end
 end
